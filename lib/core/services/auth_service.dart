9422bd292fe972aca00c768e3023a21d005f176c
Aman Miah <vfsdhaka0@gmail.com>
2026-05-06 16:32:30 +0600

    Update lib/core/services/auth_service.dart

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -19,7 +19,7 @@ class AuthService {
       final result = await _auth.authenticate(
         localizedReason: 'Unlock Browser',
         options: const AuthenticationOptions(
-          biometricOnly: true, // 🔥 force fingerprint only
+          biometricOnly: false, // 🔥 force fingerprint only
           stickyAuth: true,
           useErrorDialogs: true,
         ),
