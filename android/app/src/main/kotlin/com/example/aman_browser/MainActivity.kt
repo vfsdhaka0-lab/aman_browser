cdaa3ab75952bd24e2a326e108009d36775832c2
Aman Miah <vfsdhaka0@gmail.com>
2026-05-08 03:24:18 +0600

    Update android/app/src/main/kotlin/com/example/aman_browser/MainActivity.kt

--- android/app/src/main/kotlin/com/example/aman_browser/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/aman_browser/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.aman_browser
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
