1e4997593d96fdac3804d67d38454fe7c9a08280
Aman Miah <vfsdhaka0@gmail.com>
2026-06-01 21:26:33 +0600

    Enhance MyApp with lifecycle management and providers
    Refactor MyApp to use AuthManager for app lifecycle management and update the provider structure.

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'features/browser/tab_provider.dart';
+
 import 'features/browser/browser_screen.dart';
+import 'features/browser/tab_provider.dart';
+import 'features/auth/auth_manager.dart';
 import 'features/auth/lock_screen.dart';
 
 class MyApp extends StatefulWidget {
@@ -11,27 +13,69 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  bool unlocked = false;
+class _MyAppState extends State<MyApp>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  // 🔒 AUTO LOCK WHEN APP GOES BACKGROUND
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final auth = context.read<AuthManager>();
+
+    if (state == AppLifecycleState.paused ||
+        state == AppLifecycleState.inactive) {
+      auth.lock();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => TabProvider()),
+        ChangeNotifierProvider(
+          create: (_) => TabProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => AuthManager(),
+        ),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: unlocked
-            ? const BrowserScreen()
-            : LockScreen(
-                onSuccess: () {
-                  setState(() {
-                    unlocked = true;
-                  });
-                },
-              ),
+      child: Consumer<AuthManager>(
+        builder: (context, auth, _) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+
+            theme: ThemeData(
+              useMaterial3: true,
+              brightness: Brightness.dark,
+              colorSchemeSeed: Colors.blue,
+            ),
+
+            home: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              child: auth.isUnlocked
+                  ? const BrowserScreen(
+                      key: ValueKey("browser"),
+                    )
+                  : LockScreen(
+                      key: const ValueKey("lock"),
+                      onSuccess: () {
+                        auth.unlock();
+                      },
+                    ),
+            ),
+          );
+        },
       ),
     );
   }
-}
+} u
